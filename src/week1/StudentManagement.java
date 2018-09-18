29de1ee7cf9e10c868b11d6b9a5db6249a39eb3c
manhung99 <manhung99@users.noreply.github.com>
2018-09-18 23:35:11 +0700

    ahihii

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -8,7 +8,8 @@ public class StudentManagement {
 
     public static boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        if(s1.getGroup().equals(s2.getGroup())) return true;
+        if (s1.getGroup() == null || s2.getGroup() == null) return false;
+        if (s1.getGroup().equals(s2.getGroup())) return true;
         return false;
     }
 
@@ -16,13 +17,10 @@ public class StudentManagement {
         // TODO:
         int sbg[] = new int[numberofstudent];
 
-        for(int i=0;i<numberofstudent;i++)
-        {
-            if (sbg[i] != 1)System.out.println(students[i].getInfo());
-            for(int j=i+1;j<numberofstudent;j++)
-            {
-                if (students[j].getGroup().equals(students[i].getGroup()) && sbg[j] != 1)
-                {
+        for (int i = 0; i < numberofstudent; i++) {
+            if (sbg[i] != 1) System.out.println(students[i].getInfo());
+            for (int j = i + 1; j < numberofstudent; j++) {
+                if (students[j].getGroup().equals(students[i].getGroup()) && sbg[j] != 1) {
                     System.out.println(students[j].getInfo());
                     sbg[j] = 1;
                 }
@@ -34,21 +32,20 @@ public class StudentManagement {
     void removeStudent(String id) {
         // TODO:
         int index = -1;
-        for(int i=0;i<numberofstudent;++i) {
-            if(students[i].getId().equals(id)) {
+        for (int i = 0; i < numberofstudent; ++i) {
+            if (students[i].getId().equals(id)) {
                 index = i;
                 break;
             }
         }
-        if(index!=-1){
-            students[index]=null;
-            for(int i = index;i<numberofstudent;i++) {
-                students[i] = students[i+1];
+        if (index != -1) {
+            students[index] = null;
+            for (int i = index; i < numberofstudent; i++) {
+                students[i] = students[i + 1];
             }
-        }
-        else System.out.println("Invalid To Remove Student By This Id");
-        students[numberofstudent]=null;
-        numberofstudent--;
+            students[numberofstudent] = null;
+            numberofstudent--;
+        } else System.out.println("Invalid To Remove Student By This Id");
     }
 
     public static void main(String[] args) {
@@ -57,22 +54,22 @@ public class StudentManagement {
         Student me = new Student();
         me.setName("Nguyen Manh Hung");
         me.setId("17020788");
-        me.setGroup("INT22042");
+        me.setGroup(null);
         me.setEmail("17020788@vnu.edu.vn");
         //System.out.println(me.getName());
         //System.out.println(me.getInfo());
 
         //8
         Student s1 = new Student();
-        Student s2 = new Student("Nguyen Van A","17020998","auet@vnu.edu.vn");
-        Student s3 = new Student("Nguyen Van B","17020999","auet@vnu.edu.vn");
+        Student s2 = new Student("Nguyen Van A", "17020998", "auet@vnu.edu.vn");
+        Student s3 = new Student("Nguyen Van B", "17020999", "auet@vnu.edu.vn");
 
         /*System.out.println(s1.getInfo());
         System.out.println(s2.getInfo());
         System.out.println(s3.getInfo());*/
 
         //10
-        /*System.out.println(sameGroup(me,s2));
+        /*System.out.println(sameGroup(me, s2));
         System.out.println(sameGroup(s2,s3));*/
 
         //12
