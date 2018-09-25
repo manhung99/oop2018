16d1ae3628e6451812e1d9322d4b44c5a92f56e6
manhung99 <manhung99@users.noreply.github.com>
2018-09-25 22:57:44 +0700

    ahihii

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,30 +1,119 @@
 package week2.task2;
 
+import week2.task1.Task1;
+
 public class Fraction {
 
     // TODO: khai báo các thuộc tính
+    public int numerator, denominator;
 
     public Fraction(int numerator, int denominator) {
         // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
+        if (denominator == 0) {
+            System.out.println("Error denominator = 0");
+            return;
+        }
+        this.denominator = denominator;
+        this.numerator = numerator;
+    }
+
+    //giản ước phân số
+    public void fractionaldivisors() {
+        int gcd = Task1.gcd(this.numerator, this.denominator);
+        this.numerator /= gcd;
+        this.denominator /= gcd;
     }
 
     public Fraction add(Fraction other) {
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        if (this.denominator == 0 || other.denominator == 0) {
+            System.out.println("Error denominator = 0");
+            return null;
+        }
+        Fraction sum = new Fraction(this.numerator * other.denominator + this.denominator * other.numerator, this.denominator * other.denominator);
+        sum.fractionaldivisors();
+        return sum;
     }
 
     public Fraction subtract(Fraction other) {
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        if (this.denominator == 0 || other.denominator == 0) {
+            System.out.println("Error denominator = 0");
+            return null;
+        }
+        Fraction sub = new Fraction(this.numerator * other.denominator - this.denominator * other.numerator, this.denominator * other.denominator);
+        sub.fractionaldivisors();
+        return sub;
     }
 
     public Fraction multiply(Fraction other) {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        if (this.denominator == 0 || other.denominator == 0) {
+            System.out.println("Error denominator = 0");
+            return null;
+        }
+        Fraction mul = new Fraction(this.numerator * other.numerator, this.denominator * other.denominator);
+        mul.fractionaldivisors();
+        return mul;
     }
 
     public Fraction divide(Fraction other) {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        if (this.denominator == 0 || other.denominator == 0) {
+            System.out.println("Error denominator = 0");
+            return null;
+        }
+        else if(other.numerator==0){
+            System.out.println("Can not divided!!!");
+            return null;
+        }
+        Fraction div = new Fraction(this.numerator * other.denominator, this.denominator * other.numerator);
+        div.fractionaldivisors();
+        return div;
+    }
+
+    //hàm so sánh 2 phân số
+    public boolean equals(Fraction other){
+        if (this.denominator == 0 || other.denominator == 0) {
+            System.out.println("Error denominator = 0");
+        }
+        Fraction temp=this.subtract(other);
+        if(temp.numerator==0) return true;
+        return false;
+    }
+
+    //hàm hiển thị phân số
+    public void displayFraction(){
+        if(this.denominator==1){
+            System.out.println(this.numerator);
+        }
+        else if(this.denominator==-1){
+            System.out.println(-this.numerator);
+        }
+        else if(this.numerator==0){
+            System.out.println(0);
+        }
+        else if(this.numerator==this.denominator){
+            System.out.println(1);
+        }
+        else if(this.numerator==-this.denominator){
+            System.out.println(-1);
+        }
+        else if(this.denominator<0){
+
+            System.out.println(-this.numerator+"/"+(-this.denominator));
+        }
+        else {
+            System.out.println(this.numerator+"/"+this.denominator);
+        }
+    }
+
+    public static void main(String[] args) {
+        Fraction t1 = new Fraction(1, 2);
+        Fraction t2 = new Fraction(3, 4);
+        Fraction sum = t1.add(t2);
+        Fraction sub = t1.subtract(t2);
+        Fraction mul = t1.multiply(t2);
+        Fraction div = t1.divide(t2);
     }
 }
--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -4,3 +4,153 @@ public class Task3 {
 }
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
+
+
+//Con mèo nhà em:)
+class Cat {
+    private String color, gen;// màu sắc, kiểu gen mèo
+    private int weight;//cân nặng
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getGen() {
+        return gen;
+    }
+
+    public void setGen(String gen) {
+        this.gen = gen;
+    }
+
+    //mèo kêu
+    public void sound() {
+        System.out.println("Meo Meo");
+    }
+
+    //so sánh gen của mèo
+    public boolean samegen(Cat c1, Cat c2) {
+        if (c1.getGen() == null || c2.getGen() == null) return false;
+        if (c1.getGen().equals(c2.getGen())) return true;
+        return false;
+    }
+
+    //so sánh màu sắc của mèo
+    public boolean samecolor(Cat c1, Cat c2) {
+        if (c1.getColor() == null || c2.getColor() == null) return false;
+        if (c1.getColor().equals(c2.getColor())) return true;
+        return false;
+    }
+
+}
+
+//Cái cây nhà hàng xóm-.-
+class tree {
+    private String name, color;//tên cây,màu lá của cây
+    private int height;//chiều cao của cây
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    //so sánh chiều cao 2 cây
+    public boolean sameheight(tree t1, tree t2) {
+        if (t1.getHeight() == '\0' || t2.getHeight() == '\0') return false;
+        if (t1.getHeight() == t2.getHeight()) return true;
+        return false;
+    }
+
+    //tăng x đơn vị chiều cao cho cây
+    public void growup(int x) {
+        this.height += x;
+    }
+
+    //chuyển màu lá cho cây VD: khi sang mua thu!
+    public void changecolor(String x) {
+        this.color = x;
+    }
+}
+
+//Tài khoản ngân hàng
+class bankId {
+    public String id, name;//mã số tài khoản ngân hàng,tên chủ thẻ
+    public double balance;//số dư tài khoản
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    //cộng thêm x USD vào tài khoản và in ra số dư còn lại
+    public void addmoney(double x) {
+        this.balance += x;
+        System.out.println("Your account balance: " + balance);
+    }
+
+    //rút x USD ra khỏi tài khoản và in ra số dư còn lại
+    public void withdraw(double x) {
+        if (x > this.balance) System.out.println("Can not withdraw money");
+        else {
+            this.balance -= x;
+            System.out.println("Your account balance: " + balance);
+        }
+    }
+
+    //in ra toàn bộ thông tin tài khoản
+    public String getInfo() {
+        return "ID: " + id + '|' +
+                "Name: " + name + '|' +
+                "Balance: " + balance;
+    }
+}
