29fec7b0bedc5d61eca70a6f94f9783cf7b4b61a
manhung99 <manhung99@users.noreply.github.com>
2018-09-25 23:32:40 +0700

    ahihii lần cuối tuần 2

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,19 +4,19 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        if(b==0) return a;
-        else return gcd(b,a%b);
+        if (b == 0) return a;
+        else return gcd(b, a % b);
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fibonacci(n-1) + fibonacci(n-2);
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
     public static void main(String[] args) {
-        System.out.println(gcd(27,5));
+        System.out.println(gcd(27, 5));
         System.out.println(fibonacci(9));
     }
 }
