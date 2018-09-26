39f869e5aca326300c8f4bf6d998d31ff5a0ff17
manhung99 <manhung99@users.noreply.github.com>
2018-09-26 21:35:55 +0700

    ahihii lần cuối tuần 2

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -1,25 +1,51 @@
 package week3;
 
 public class Week3 {
-
+    /**
+     * Chương trình tìm giá trị lớn nhất của hai số nguyên
+     *
+     * @param m số nguyên thứ nhất
+     * @param n số nguyên thứ hai
+     * @return số lớn nhất trong của hai số
+     */
     public static int max(int m, int n) {
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        if (m > n) return m;
+        return n;
     }
 
+    /**
+     * Chương trình tìm giá trị nhỏ nhất của của một mảng số nguyên
+     *
+     * @param array mảng cần tìm
+     * @return giá trị nhỏ nhất của mảng
+     */
     public static int minOfArray(int[] array) {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        int minofA = 999999;
+        for (int i = 0; i < 5; i++) if (array[i] < minofA) minofA = array[i];
+        return minofA;
     }
 
     /**
      * Chương trình tính chỉ số BMI và in ra kết quả đánh giá
+     *
      * @param weight cân nặng
      * @param height chiều cao
      * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+        double BMI = weight / height / height;
+        if (BMI < 18.5) return "Thiếu cân";
+        else if (BMI >= 18.5 && BMI < 23) return "Bình thường";
+        else if (BMI >= 23 && BMI < 25) return "Thừa cân";
+        return "Béo phì";
+    }
+
+    public static void main(String[] args) {
+        int[] ar = {1,2,-39,4,5};
+        System.out.println(minOfArray(ar));
+        System.out.println(calculateBMI(57,1.66));
     }
 }
