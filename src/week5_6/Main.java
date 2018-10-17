eceaa88ac6b821baeff9b2f7b42fc30dabbdbdc9
manhung99 <manhung99@users.noreply.github.com>
2018-10-17 13:51:10 +0700

    update

---
+++ src/week5_6/Main.java
@@ -0,0 +1,46 @@
+package week5_6;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        Circle circle1 = new Circle();
+        Rectangle rectangle = new Rectangle();
+        Rectangle rectangle1 = new Rectangle();
+        Square square = new Square();
+        Square square1 = new Square();
+        Triangle triangle = new Triangle();
+        Triangle triangle1 = new Triangle();
+        Hexagon hexagon = new Hexagon();
+        Hexagon hexagon1 = new Hexagon();
+        Layer layer = new Layer();
+        Diagram diagram = new Diagram();
+
+        layer.getLayer().add(rectangle);
+        layer.getLayer().add(circle);
+        layer.getLayer().add(square);
+        layer.getLayer().add(hexagon1);
+        layer.getLayer().add(circle1);
+        layer.getLayer().add(rectangle1);
+        layer.getLayer().add(hexagon);
+        layer.getLayer().add(triangle);
+        layer.getLayer().add(square1);
+        layer.getLayer().add(triangle1);
+
+
+        System.out.println(circle.getColor());
+        //layer.removeTriangle();
+        //layer.revomeDuplicated();
+        //layer.removeCircle();
+        //layer.printLayer();
+
+
+
+        diagram.getDiagram().add(layer);
+        //diagram.removeCircle();
+
+
+        diagram.sortLayer();
+        diagram.printDiagram();
+
+    }
+}
